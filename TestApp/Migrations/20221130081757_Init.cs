74baea4d6f38b8a14b7f2ce45db2354f7d36c3e2
editim <edijs.timosenko@jeanedwards.com>
2022-11-30 10:20:47 +0200

    init .net 6

---
+++ TestApp/Migrations/20221130081757_Init.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestApp.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    task_type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TaskA_RandomProp = table.Column<int>(type: "int", nullable: true),
+                    TaskAProp = table.Column<int>(type: "int", nullable: true),
+                    RandomProp = table.Column<int>(type: "int", nullable: true),
+                    TaskBProp = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using (var dbContext = new MyDbContext())
+{
+    dbContext.Database.EnsureDeleted();
+    dbContext.Database.EnsureCreated();
+
+    var taskA = new TaskA { Name = "Task A", RandomProp = 123, TaskAProp = 2 };
+    var taskB = new TaskB { Name = "Task B", RandomProp = 123, TaskBProp = 415 };
+
+    dbContext.Tasks.Add(taskA);
+    dbContext.Tasks.Add(taskB);
+
+    dbContext.SaveChanges();
+
+    var list = dbContext.Tasks.ToList();
+}
+
+
+
+public class MyDbContext : DbContext
+{
+    public DbSet<TaskBase> Tasks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TaskBase>()
+            .HasDiscriminator<string>("task_type")
+            .HasValue<TaskA>("a")
+            .HasValue<TaskB>("b");
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=TBH_test;MultipleActiveResultSets=true;Integrated Security=True;Encrypt=False;");
+
+        base.OnConfiguring(optionsBuilder);
+    }
+}
+
+
+public abstract class TaskBase
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class TaskA : TaskBase
+{
+    public int RandomProp { get; set; }
+    public int TaskAProp { get; set; }
+}
+
+public class TaskB : TaskBase
+{
+    public int RandomProp { get; set; }
+    public int TaskBProp { get; set; }
+}
